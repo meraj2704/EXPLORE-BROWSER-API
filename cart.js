015d23212aa7a65a2326965169f88f4aa71fa11f
IJE2704 <mh1669101@gmail.com>
2023-07-04 19:16:23 +0600

    local storage

--- cart.js
+++ cart.js
@@ -1,7 +1,9 @@
+
 const addProduct = () =>{
     const Product = get('product-name');
     const quantity = get('product-quantity');
-    localStorage.setItem(Product,quantity);
+    // localStorage.setItem(Product,quantity);
+    saveItemLocalStorage(Product,quantity);
     display(Product,quantity);
 }
 const get = id =>{
@@ -16,3 +18,27 @@ const display =(Product,quantity) =>{
     li.innerText = `${Product} : ${quantity}`;
     displayContainer.appendChild(li);
 }
+const getshopingCartLocalStorage = () =>{
+    let saceCart = localStorage.getItem('cart');
+    let cart = {};
+    if(saceCart){
+        cart = JSON.parse(saceCart);
+    }
+    return cart;
+}
+const saveItemLocalStorage = (product,quantity)=>{
+    const cart = getshopingCartLocalStorage();
+    cart[product] = quantity;
+    const cartstringify = JSON.stringify(cart);
+    localStorage.setItem('cart', cartstringify);
+}
+const displayStoreProduct =()=>{
+    const cart = getshopingCartLocalStorage();
+    for(const product in cart)
+    {
+        const quantity = cart[product];
+        console.log(cart,product);
+        display(product,quantity);
+    }
+}
+displayStoreProduct;
